7376b5d0e861e938ed8fa46ecea1525af83c5492
Rohit Patwa <therohitpatwa@gmail.com>
2025-08-05 16:31:15 +0530

    First Version of numsy package

---
+++ src/main/java/com/therohitpatwa/numsy/Numsy.java
@@ -0,0 +1,65 @@
+package com.therohitpatwa.numsy;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Numsy {
+    public static boolean isEven(int n) {
+        return n%2==0;
+    }
+    public static boolean isOdd(int n) {
+        return n%2!=0;
+    }
+    public static boolean isPrime(int n) {
+        if(n<=1) return false;
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    public static List<Integer>  primeInRange(int start,int end)
+    {
+        List<Integer> primes=new ArrayList<>();
+        for(int i=Math.max(2,start);i<=end;i++)
+        {
+            if(isPrime(i))
+            {
+                primes.add(i);
+            }
+        }
+        return primes;
+    }
+
+    public static long factorial(int n)
+    {
+        long result=1;
+        int fact=1;
+        if(n==0)
+            return 1;
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        return fact;
+    }
+    public static String formatWithCommas(long n)
+    {
+        return NumberFormat.getNumberInstance(Locale.US).format((n));
+    }
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return Math.abs(a);
+    }
+    public static int lcm(int a, int b) {
+        return Math.abs(a * b)/gcd(a,b);
+    }
+
+}
